ea4c0f0b650204f28839656babe4f9a0a6c34b1d
siberiawolf <siberiawolf@126.com>
2015-07-03 22:05:02 +0800

    test

--- src/com/siberiawolf/SiberiawolfApplication.java
+++ src/com/siberiawolf/SiberiawolfApplication.java
@@ -2,13 +2,14 @@ package com.siberiawolf;
 
 import com.jiuli.library.LibraryApplication;
 import com.jiuli.library.comm.LibraryComm;
+import com.siberiawolf.com.Comm;
 
 public class SiberiawolfApplication extends LibraryApplication {
 
 	@Override
 	public String getOwnCacheDirectory() {
 		// TODO Auto-generated method stub
-		return LibraryComm.SDCARD_ROOT;
+		return Comm.SDCARD_IMG_ROOT;
 	}
 
 }
---
+++ src/com/siberiawolf/com/Comm.java
@@ -0,0 +1,8 @@
+package com.siberiawolf.com;
+
+import com.jiuli.library.comm.LibraryComm;
+
+public class Comm extends LibraryComm {
+
+	public static final String SDCARD_IMG_ROOT = SDCARD_ROOT+"/siberiawolf/img";
+}
